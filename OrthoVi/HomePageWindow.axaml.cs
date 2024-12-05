801fb1309e550c96a835cd9d83abf7c2cc93aa17
Raya Sokolova <raya.sok2007@gmail.com>
2024-12-05 23:05:24 +0200

    Added the home page. Added style file for the images. Changed the ButtonStyles file. Added the ability the window to be draged.

---
+++ OrthoVi/HomePageWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace OrthoVi;
+
+public partial class HomePageWindow : Window
+{
+    public HomePageWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+        var draggableArea = this.FindControl<Border>("DraggableArea");
+        draggableArea.PointerPressed += DraggableArea_PointerPressed;
+    }
+    private void DraggableArea_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            // Begin dragging the window only if clicked within the draggable area
+            BeginMoveDrag(e);
+        }
+    }
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+
+    private void MinimizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Minimize the window
+        this.WindowState = WindowState.Minimized;
+    }
+}
--- OrthoVi/MainWindow.axaml.cs
+++ OrthoVi/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using System;
@@ -9,14 +11,37 @@ namespace OrthoVi
     {
         public MainWindow()
         {
+
            InitializeComponent();
-           
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var draggableArea = this.FindControl<Border>("DraggableArea");
+            draggableArea.PointerPressed += DraggableArea_PointerPressed;
         }
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
 
+        private void MinimizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            // Minimize the window
+            this.WindowState = WindowState.Minimized;
+        }
 
+        private void DraggableArea_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                // Begin dragging the window only if clicked within the draggable area
+                BeginMoveDrag(e);
+            }
+        }
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomePageWindow homeWindow = new HomePageWindow();
+            homeWindow.Show();
+        }
     }
 }
