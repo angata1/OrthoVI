316cf466915eb4ea0eb55c947f84d43451a58822
Raya Sokolova <raya.sok2007@gmail.com>
2025-01-26 20:52:46 +0200

    Added SavefileHander class. Added methods to MainWindow for log in and sign up. Also installed the package MessageBox.Avalonia for message boxes

--- OrthoVi/MainWindow.axaml.cs
+++ OrthoVi/MainWindow.axaml.cs
@@ -4,6 +4,9 @@ using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using System;
+using System.Text;
+using MsBox.Avalonia.Enums;
+using MsBox.Avalonia;
 
 namespace OrthoVi
 {
@@ -12,7 +15,7 @@ namespace OrthoVi
         public MainWindow()
         {
 
-           InitializeComponent();
+            InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
@@ -38,11 +41,76 @@ namespace OrthoVi
                 BeginMoveDrag(e);
             }
         }
-        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        private async void LogInButton_Click(object sender, RoutedEventArgs e)
         {
-            HomePageWindow homeWindow = new HomePageWindow();
-            homeWindow.Show();
-            this.Hide();
+            var dbManager = new DatabaseManager();
+            string logInUsername = UsernameLogInTextBox.Text;
+            string logInPassword = PasswordLogInTextBox.Text;
+            if (logInUsername != null && logInPassword != null)
+            {
+                var user = dbManager.ReadDatabase(logInUsername, logInPassword);
+                if (user != null)
+                {
+                    // Show the message box dialog
+                     var box = MessageBoxManager
+                    .GetMessageBoxStandard("Log In Successful", "You successfully logged in your account!", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Success);
+
+                     var result = await box.ShowWindowAsync();
+
+                     HomePageWindow homeWindow = new HomePageWindow();
+                     homeWindow.Show();
+                     this.Hide();
+                }
+                else
+                {
+                    var box = MessageBoxManager
+                    .GetMessageBoxStandard("Error", "The username or password is wrong", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+
+                    var result = await box.ShowWindowAsync();
+                }
+               
+            }
+            else
+            {
+                var box = MessageBoxManager
+                .GetMessageBoxStandard("Error", "Required fields are empty. Please enter all details to continue.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+
+                var result = await box.ShowWindowAsync();
+            }
+           
         }
+        private async void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dbManager = new DatabaseManager();
+
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            string doctorFirstName = FistNameTextBox.Text;
+            string doctorLastName = LastNameTextBox.Text;
+            byte[] profilePicture = Encoding.UTF8.GetBytes("ProfilePicturePlaceholder");
+
+            if (username != null && password != null && doctorFirstName != null && doctorLastName != null)
+            {
+                dbManager.CreateDatabase(username, password, doctorFirstName, doctorLastName, profilePicture);
+
+                // Show the message box dialog
+                var box = MessageBoxManager
+                .GetMessageBoxStandard("Creation Successful", "Account was created!", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Success);
+
+                 var result = await box.ShowWindowAsync(); 
+            }
+            else
+            {
+                var box = MessageBoxManager
+                .GetMessageBoxStandard("Error", "Required fields are empty. Please enter all details to continue.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+
+                var result = await box.ShowWindowAsync();
+            }
+
+
+        }
+
+
+
     }
 }
---
+++ OrthoVi/SaveFileHander.cs
@@ -0,0 +1,201 @@
+﻿// File: DatabaseManager.cs
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseManager
+{
+    public static string mainPath = "./SaveFiles/";
+
+    public DatabaseManager() {
+        if (Directory.Exists(mainPath))
+        {
+
+        }
+        else {
+        Directory.CreateDirectory(mainPath);
+        }
+    }
+
+    public void CreateDatabase(string username, string password, string DoctorFirstName, string DoctorLastName, byte[] profilePicture)
+    {
+        string databaseFile = $"{mainPath}{username}.db";
+        if (File.Exists(databaseFile))
+        {
+            Console.WriteLine("Database already exists.");
+            return;
+        }
+
+        using (var context = new UserDbContext(databaseFile))
+        {
+            context.Database.EnsureCreated();
+            var user = new User
+            {
+                Username = username,
+                Password = password,
+                DoctorInformation = new DoctorInformation
+                {
+                    Firstname = DoctorFirstName,
+                    Lastname = DoctorLastName,
+                    ProfilePicture = Convert.ToBase64String(profilePicture),
+                    ProjectHistory = new List<Project>()
+                }
+            };
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+    }
+
+    public User ReadDatabase(string username, string password)
+    {
+        string databaseFile = $"{mainPath}{username}.db";
+        if (!File.Exists(databaseFile))
+        {
+            throw new FileNotFoundException("Database file not found.");
+        }
+
+        using (var context = new UserDbContext(databaseFile))
+        {
+            var user = context.Users.Include(u => u.DoctorInformation)
+                                     .ThenInclude(d => d.ProjectHistory)
+                                     .ThenInclude(p => p.Client)
+                                     .ThenInclude(c => c.Image)
+                                     .ThenInclude(i => i.Annotation)
+                                     .ThenInclude(a => a.Coordinates)
+                                     .FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user == null)
+            {
+                Console.WriteLine("Incorrect username or password");
+                return null;
+            }
+
+            return user;
+        }
+    }
+
+    public void UpdateDatabase(string username, User user)
+    {
+        string databaseFile = $"{mainPath}{username}.db";
+        if (!File.Exists(databaseFile))
+        {
+            throw new FileNotFoundException("Database file not found.");
+        }
+
+        using (var context = new UserDbContext(databaseFile))
+        {
+            context.Users.Update(user);
+            context.SaveChanges();
+        }
+    }
+
+    public (string Username, string Password) GetUserCredentials(string username)
+    {
+        string databaseFile = $"{mainPath}{username}.db";
+        if (!File.Exists(databaseFile))
+        {
+            throw new FileNotFoundException("Database file not found.");
+        }
+
+        using (var context = new UserDbContext(databaseFile))
+        {
+            var user = context.Users.FirstOrDefault();
+            if (user == null) throw new Exception("User not found in the database.");
+
+            return (user.Username, user.Password);
+        }
+    }
+}
+
+// DbContext and Entity Models
+public class UserDbContext : DbContext
+{
+    private readonly string _databaseFile;
+
+    public UserDbContext(string databaseFile)
+    {
+        _databaseFile = databaseFile;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<DoctorInformation> DoctorInformations { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ClientInformation> ClientInformations { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<ImageAnnotation> ImageAnnotations { get; set; }
+    public DbSet<Coordinates> Coordinates { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={_databaseFile}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Username);
+        modelBuilder.Entity<DoctorInformation>().HasKey(d => d.DoctorInformationId);
+        modelBuilder.Entity<Project>().HasKey(p => p.ProjectId);
+        modelBuilder.Entity<ClientInformation>().HasKey(c => c.ClientInformationId);
+        modelBuilder.Entity<Image>().HasKey(i => i.ImageId);
+        modelBuilder.Entity<ImageAnnotation>().HasKey(ia => ia.ImageAnnotationId);
+        modelBuilder.Entity<Coordinates>().HasKey(c => c.CoordinatesId);
+    }
+}
+
+// Entities
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public DoctorInformation DoctorInformation { get; set; }
+}
+
+public class DoctorInformation
+{
+    public int DoctorInformationId { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string ProfilePicture { get; set; }
+    public List<Project> ProjectHistory { get; set; }
+}
+
+public class Project
+{
+    public int ProjectId { get; set; }
+    public int? ClientInformationId { get; set; }
+    public ClientInformation Client { get; set; }
+}
+
+public class ClientInformation
+{
+    public int ClientInformationId { get; set; }
+    public string ClientFirstName { get; set; }
+    public string ClientLastName { get; set; }
+    public string Gender { get; set; }
+    public int ClientAge { get; set; }
+    public List<Image> Image { get; set; }
+}
+
+public class Image
+{
+    public int ImageId { get; set; }
+    public string ImageName { get; set; }
+    public byte[] ImageContent { get; set; }
+    public List<ImageAnnotation> Annotation { get; set; }
+}
+
+public class ImageAnnotation
+{
+    public int ImageAnnotationId { get; set; }
+    public string LandmarkName { get; set; }
+    public Coordinates Coordinates { get; set; }
+}
+
+public class Coordinates
+{
+    public int CoordinatesId { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
